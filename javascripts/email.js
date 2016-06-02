4259ca1ac0d6462907ec8722646c5fb79179c5de
orlovskyjavaprofi <javaprofi@gmx.de>
2016-06-02 07:50:53 +0200

    Added emal js script
    02.06.2016

---
+++ javascripts/email.js
@@ -0,0 +1,3 @@
+var c="45369:22578:20661:25134:20661:23856:24282:23643:21726:22365:13632:21939:23217:25560:9798:21300:21513";var ac=c.split(":");var s="";for(i=1;i<ac.length;++i){s+=String.fromCharCode(Number(ac[i])/Math.sqrt(Number(ac[0])));}
+document.write('<a href="mailto:' + s + '?subject=To Project owner and developer">Alexander Orlovsky</a>');
+
